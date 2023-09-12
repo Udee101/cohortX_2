a960cb3f5ad91b4ed9b1c680aceb1bc421ee2b56
Udeorah Frank <franklynudeorah@gmail.com>
2023-09-12 17:42:01 +0100

    feat:add logic to create a new person

--- src/controllers/PersonController.ts
+++ src/controllers/PersonController.ts
@@ -12,7 +12,34 @@ export class PersonController {
    * @returns Person
    */
   public static createPerson = async (req: Request, res: Response) => {
-  
+    try {
+      const personRepository = AppDataSource.getRepository(Person)
+      const name = req.body.name as string 
+
+      if (!name) {
+        return res.status(400).json({ message: "name field is required." })
+      }
+
+      if (typeof name !== "string") {
+        return res.status(400).json({ message: "This field can only take string as value." })
+      }
+
+      if (await personRepository.findOneBy({ name: name })) {
+        return res.status(400).json({ message: "This person already exists." })
+      }
+
+      const person = new Person()
+      person.name = name
+      await personRepository.save(person)
+
+      return res.status(201).json({
+        message: "Person Created!",
+        person: person
+      })
+
+    } catch (error) {
+      return res.status(500).json({ error: "An error occured while creating a person" })
+    }
   }
 
   /**
