281cfe96faaa959de37df54de003c6f3500de2ab
Udeorah Frank <franklynudeorah@gmail.com>
2023-09-12 17:30:50 +0100

    update:get db config from .env file

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,17 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { Person } from "./models/Person"
+import * as dotenv from "dotenv"
+
+dotenv.config()
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 5432,
-    username: "postgres",
-    password: "test1234",
-    database: "stage2_db",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: true,
     logging: false,
     entities: [Person],
