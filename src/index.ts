f8a6d2b3f50c99f144e56d6245995bcbd87732c5
Udeorah Frank <franklynudeorah@gmail.com>
2023-09-13 12:14:16 +0100

    handle invalid route or params

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,9 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api', router)
+app.use((req: express.Request, res: express.Response) => {
+    res.status(404).json({ error: "API route no found or Invalid parameters passed" })
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
